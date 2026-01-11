5c469b9fc9613ae6a8525afc85506566ba429a61
matthewwcummings <matthewwcummings@gmail.com>
2026-01-10 20:24:31 -0500

    update package dependencies and configurations

--- src/server/api/routers/gemini.ts
+++ src/server/api/routers/gemini.ts
@@ -10,10 +10,27 @@ export const geminiRouter = createTRPCRouter({
   testKey: publicProcedure
   .input(z.object({text: z.string()}))
   .query(async({ input }) => {
-
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-    const result = await model.generateContent(input.text);
-    console.log("hi", result);
+    const model = genAI.getGenerativeModel({ 
+      model: "gemini-2.5-flash",
+      systemInstruction: `
+      You are an AI assistant that generates homework questions for students based on their information.
+`
+    });
+    const result = await model.generateContent({
+      contents: [
+        {
+          role: "user",
+          parts: [
+            {
+              text: `Class: Grade 3 Algebra,
+              Student Information: ${input.text},
+              Task: Generate ONE homework question suitable for this student, tailor to their skillset and interests.
+              Delivery: Include only the question in your response without any additional text, plain text format only`,
+            },
+          ]
+        }
+      ]
+    });
 
     return {
       text: result.response.text(),
